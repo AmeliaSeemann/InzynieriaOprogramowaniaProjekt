20548ca40965834c0db60aeee72a886d219e9675
Zuzina-Zieba <zuzia.k.zieba@gmail.com>
2025-11-25 18:52:54 +0100

    Poprawienie/ulepszenie odczytu obrazów i wykrywania krawędzi

--- photos_opencv.py
+++ photos_opencv.py
@@ -19,19 +19,33 @@ def get_contours(photo):
 
 #zwraca koordynaty potrzebne do przycięcia zdjęć z przezroczystym tłem
 def get_crop(photo):
-    #znalezienie konturów
-    contours_x, contours_y = get_contours(photo)
-    #znalezienie ich skrajnych wartości
-    max_x = max(contours_x)[0]
-    max_y = max(contours_y)[0]
-    min_x = min(contours_x)[0]
-    min_y = min(contours_y)[0]
+    img = cv.imread(photo, cv.IMREAD_UNCHANGED)
+    if img is None:
+        return None     # plik nie istnieje lub nie jest zdjęciem
 
-    #dostosowanie skrajnych wartości jeżeli nie wykryto lewego górnego rogu
-    if max_x == min_x and max_x != 0:
-        min_x = 0
-    if max_y == min_y and max_y != 0:
-        min_y = 0
+    # obraz ma kanał alfa = PNG z przezroczystością
+    if img.shape[2] == 4:
+        # wyciągamy kanał alfa (odpowiada za przezroczystość)
+        alpha = img[:, :, 3]
 
-    #zwrócenie ich w odpowiedniej kolejności przydatnym do przycięcia QPixMap
-    return min_x, min_y, max_x - min_x, max_y - min_y
+        # tworzymy binarną maskę:
+        # piksele > 1 traktujemy jako należące do kształtu
+        _, thresh = cv.threshold(alpha, 1, 255, cv.THRESH_BINARY)
+    else:
+        # brak kanału alfa - konwertujemy do grayscale
+        gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        _, thresh = cv.threshold(gray, 250, 255, cv.THRESH_BINARY_INV)
+
+    # znajduje kontury
+    contours, _ = cv.findContours(thresh, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+
+    if not contours:
+        return None       # nic nie znaleziono - prawdopodobnie puste zdjęcie
+
+    # wybieramy największy kontur, zakładając że jest właściwym obiektem
+    cnt = max(contours, key=cv.contourArea)
+
+    # pobieramy minimalny prostokąt otaczający kontur
+    x, y, w, h = cv.boundingRect(cnt)
+
+    return x, y, w, h
